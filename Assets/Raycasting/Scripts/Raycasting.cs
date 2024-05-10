3adc22536b97f320f33005d385024b179b44e745
TK742 <ttasca742@gmail.com>
2024-05-10 12:02:26 -0300

    sync

--- Assets/Raycasting/Scripts/Raycasting.cs
+++ Assets/Raycasting/Scripts/Raycasting.cs
@@ -11,9 +11,12 @@ public class Raycasting : MonoBehaviour
     
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), ))
+        if (Input.GetMouseButtonDown(0) && // Si se detecta el click del mouse
+            Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+            // Castear un rayo hacia la posición del map. Si hay colisión, ingresa al condicional.
         {
-            
+            Debug.Log($"{hit.point} es la posición del hit");
+            cube.position = hit.point;
         }
     }
 }
